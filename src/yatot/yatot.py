ea1c99fecd811bd45b475eee4f9ac80c4b20c857
kcousot <kevin.cousot@gmail.com>
2014-01-13 00:23:06 +0100

    Ajout d'un callback quand un indice est donné

--- src/yatot/yatot.py
+++ src/yatot/yatot.py
@@ -17,10 +17,35 @@ import logging as log
 import networkx as nx
 
 class Yatot:
-    
+
+
     def __init__(self, db):
         self._db = db
         self._cli = cli.CLI(self)
+        self._hints = []
+
 
     def play(self):
         self._cli.cmdloop()
+
+
+    def _hintGiven(self, hint):
+        self._hints.append(hint)
+        nodeID = self._db.queryNodeIDByName(hint)
+        if len(nodeID) == 0:
+            self._cli.printMsg("Unknown word : {0}".format(hint))
+            return
+        if len(nodeID) > 1:
+            log.debug("There are more than one node with name: %s. " \
+                      "Picking the first one : %s",
+                      hint, nodeID[0][0])
+        nodeID = nodeID[0][0]
+        nodeDatas = self._db.queryNodeDatas(nodeID)
+        self._cli.printRow(nodeDatas)
+        neighboursIDs = self._db.queryNeighboursByID(nodeID)
+        neighboursDatas = {}
+        for elt in neighboursIDs:
+            neighbourID = elt["neighbour"]
+            neighboursDatas[neighbourID] = self._db.queryNodeDatas(neighbourID)
+        print neighboursDatas
+
