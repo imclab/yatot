6bb96ea85ad1bb76db984cf43e61d0a3b2299287
kcousot <kevin.cousot@gmail.com>
2014-01-13 00:22:41 +0100

    Ajout d'un filtre sur type de noeuds et relations. Ajout de requetes

--- src/yatot/sqliteDB.py
+++ src/yatot/sqliteDB.py
@@ -23,6 +23,16 @@
 import logging as log
 import sqlite3
 
+FILTERED_NODETYPES = [6, 7, 8, 9, 10, 18, 36, 666]
+FILTERED_RELATIONTYPES = [12, 29, 29, 45, 46, 47, 48, 66, 105, 666, 777, 997, \
+                          998, 1000, 1001, 1002, 2001]
+
+
+def listGen(l):
+    for element in l:
+        yield (element,)
+
+
 class SQLiteDB:
 
 
@@ -31,8 +41,17 @@ class SQLiteDB:
         self._con = sqlite3.connect(self._path)
         if self._isConnected():
             log.info("Connected to %s", self._path)
+            self._con.row_factory = sqlite3.Row
             if foreignKeysPragma:
                 self._turnOnFK()
+            log.debug("Creating table filteredNodeTypes")            
+            self._createFilteredNodeTypesTbl()
+            log.debug("Creating table filteredRelationTypes")
+            self._createFilteredRelationTypesTbl()
+            log.debug("Setting node type filters")
+            self.setFilteredNodeTypes(FILTERED_NODETYPES)
+            log.debug("Setting relation type filters")
+            self.setFilteredRelationTypes(FILTERED_RELATIONTYPES)
         else:
             msg = "Could not connect to {0}".format(self._path)
             log.critical(msg)
@@ -57,15 +76,69 @@ class SQLiteDB:
         cur.execute("PRAGMA foreign_keys = ON")
 
 
+    def _createFilteredNodeTypesTbl(self):
+        cur = self._con.cursor()
+        script = "DROP TABLE IF EXISTS filteredNodeTypes; "              \
+                 "CREATE TABLE filteredNodeTypes ( "                     \
+                 "    ntType INTEGER, "                                  \
+                 "    FOREIGN KEY(ntType) REFERENCES nodeTypes(ntType) " \
+                 ");"
+        cur.executescript(script)
+
+
+    def _createFilteredRelationTypesTbl(self):
+        cur = self._con.cursor()
+        script = "DROP TABLE IF EXISTS filteredRelationTypes; "              \
+                 "CREATE TABLE filteredRelationTypes ( "                     \
+                 "    rtType INTEGER, "                                      \
+                 "    FOREIGN KEY(rtType) REFERENCES relationTypes(rtType) " \
+                 ");"
+        cur.executescript(script)
+
+
+    def setFilteredNodeTypes(self, types):
+        cur = self._con.cursor()
+        script = "INSERT INTO filteredNodeTypes VALUES (?)"
+        cur.executemany(script, listGen(FILTERED_NODETYPES))
+        self._con.commit()
+
+
+    def setFilteredRelationTypes(self, types):
+        cur = self._con.cursor()
+        script = "INSERT INTO filteredRelationTypes VALUES (?)"
+        cur.executemany(script, listGen(FILTERED_RELATIONTYPES))
+        self._con.commit()
+
+
+    def queryNodeIDByName(self, name):
+        cur = self._con.cursor()
+        cur.execute("SELECT nID FROM nodes " \
+                    "WHERE  nName = :name",
+                    {"name" : name})
+        return cur.fetchall()
+
+
     def queryNeighboursByID(self, nID):
         cur = self._con.cursor()
-        cur.execute("SELECT rTo AS neighbour "   \
-                    "FROM relations "            \
-                    "WHERE rFrom = :rFrom "      \
-                    "UNION "                     \
-                    "SELECT rFrom AS neighbour " \
-                    "FROM relations "            \
-                    "WHERE rTo = :rTo",
+        cur.execute("SELECT DISTINCT R.rTo AS neighbour "            \
+                    "FROM relations AS R, "                          \
+                    "     nodes     AS N "                           \
+                    "WHERE R.rFrom = :rFrom "                        \
+                    "AND   N.nID   = R.rTo "                         \
+                    "AND   N.nType NOT IN "                          \
+                    "  (SELECT ntType FROM filteredNodeTypes) "      \
+                    "AND   R.rType NOT IN "                          \
+                    "  (SELECT rtType FROM filteredRelationTypes) "  \
+                    "UNION "                                         \
+                    "SELECT DISTINCT R.rFrom AS neighbour "          \
+                    "FROM relations AS R, "                          \
+                    "     nodes     AS N "                           \
+                    "WHERE R.rTo  = :rTo "                           \
+                    "AND   N.nID  = R.rFrom "                        \
+                    "AND   N.nType NOT IN "                          \
+                    "  (SELECT rtType FROM filteredRelationTypes) "  \
+                    "AND   R.rType NOT IN "                          \
+                    "  (SELECT rtType FROM filteredRelationTypes) ",
                     {"rFrom" : nID, "rTo" : nID})
         return cur.fetchall()
 
